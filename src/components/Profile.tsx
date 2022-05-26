b4d999c66d4e93ded0f4d298e091267608571cf3
gabeogard <gaog001@student.kristiania.no>
2022-05-26 16:24:48 +0200

    init

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,7 @@
 export const Profile = () =>{
     return(
         <div className={"profile"}>
-            <img src="/src/components/me.png" className={"img"} alt="picture_of_me"/>
+            <img src="https://i.ibb.co/0cRXn8F/me.jpg" className={"img"} alt="picture_of_me"/>
             <div className={"contact-icons"}>
                 <a href={"https://www.linkedin.com/in/gabriel-ogard"} target={"_blank"}><i className="devicon-linkedin-plain contact-icon"></i></a>
                 <a href={"https://github.com/gabeogard"} target={"_blank"}><i className="devicon-github-original contact-icon"></i></a>
