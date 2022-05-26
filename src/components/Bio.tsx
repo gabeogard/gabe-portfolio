5af4e392b4a4d4f255f6271a844593f2793661a2
gabeogard <gaog001@student.kristiania.no>
2022-05-26 16:37:27 +0200

    init

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -8,7 +8,7 @@ export const Bio = () =>{
                     BSc Degree in IT (Specialization in Frontend and Mobile Development).
                     I <strong>love</strong> to write code and learn new techniques to solving problems.
                     Most of the projects I am currently working on contain some aspect of Kotlin for Backend (or Mobile app)
-                    and or some form of JavaScript framework written in TypeScript for the Frontend / Web side of things.
+                    and / or some form of JavaScript framework written in TypeScript for the Frontend / Web side of things.
                     <br/>
                     <br/>
                     <strong>PROJECTS SECTION COMING SOON.. WIP</strong>
