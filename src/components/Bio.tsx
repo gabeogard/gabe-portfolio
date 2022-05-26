a36b8ed52a3a2b61857dd0748ce30a12d49c2d54
gabeogard <gaog001@student.kristiania.no>
2022-05-26 16:36:17 +0200

    init

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -6,7 +6,7 @@ export const Bio = () =>{
                 <p className={"bio"}>
                     My name is Gabriel Øgård and I am a Fullstack Software Engineer with a
                     BSc Degree in IT (Specialization in Frontend and Mobile Development).
-                    I passionately <strong>love</strong> to write code and learn new techniques to solve problems.
+                    I <strong>love</strong> to write code and learn new techniques to solve problems.
                     Most of the projects I am currently working on contain some aspect of Kotlin for Backend (or Mobile app)
                     and or some form of JavaScript framework written in TypeScript for the Frontend / Web side of things.
                     <br/>
